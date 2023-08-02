cad6c8edcdd942b69c3ea1d3ce25a0d162b3d155
Long Ngo <126145913+NVCLong@users.noreply.github.com>
2023-08-02 08:33:17 +0700

    update search feature

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -15,8 +15,21 @@ class SiteController {
   }
   // [GET} /search
 
-  search(req, res) {
-    res.render('search');
+  async search(req, res) {
+    try{
+      Course.find({keyword: { $regex: req.body.name.toLowerCase() }})
+          .then(function (courses) {
+            // res.json({courses:courses})
+            res.render('search',{courses:until.multipleMongooseToObject(courses)})
+          })
+          .catch(function (reason) {
+            console.log(reason)
+            res.json({msg:"error"})
+          })
+    }catch(e){
+      console.log(e)
+      res.json({e:e})
+    }
   }
 }
 module.exports = new SiteController();
